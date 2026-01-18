85f00dedac17ebb2f90807ec47c8c8552978882d
Viktoriia <viktoriamyhailiak@gmail.com>
2026-01-18 17:54:15 +0200

    add a solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,54 +1,39 @@
-/* eslint-disable max-len */
-const user = {
-  username: 'Vikam',
-  email: 'vika1@test.com',
-  password: 'vika'
-};
+describe('Article Flow', () => {
+  const timestamp = Date.now();
 
-describe('Create the article', () => {
+  const user = {
+    username: `Vika${timestamp}`,
+    email: `vika${timestamp}@test.com`,
+    password: 'Password123!'
+  };
+
+  const article = {
+    title: `JavaScript ${timestamp}`,
+    description: `Test Description ${timestamp}`,
+    body: `Test Body ${timestamp}`
+  };
+
   before(() => {
-    cy.visit('/');
-
-    cy.contains('a.nav-link', 'Sign in').click();
-    cy.get('[type=email]').type(user.email);
-    cy.get('[type=password]').type(user.password);
-    cy.get('.btn-lg').click();
+    cy.login(user.email, user.username, user.password);
   });
 
   it('should create an article', () => {
-    cy.contains('a.nav-link', 'New Article').click();
-
-    cy.get('[placeholder="Article Title"]').type('JavaScript');
-    // eslint-disable-next-line quotes
-    cy.get(':nth-child(2) > .form-control').type('JavaScript');
-    cy.get('textarea.form-control').type('JavaScript');
-    cy.get('[placeholder="Enter tags"]').type('laguage');
-    cy.get('.btn-lg').click();
-    cy.get('.btn-lg').click();
-    cy.contains('h1', 'JavaScript').should('be.visible');
-  });
-});
-
-describe('Delete the article', () => {
-  before(() => {
+    cy.createArticle(article.title, article.description, article.body);
     cy.visit('/');
-
-    cy.contains('a.nav-link', 'Sign in').click();
-    cy.get('[type=email]').type(user.email);
-    cy.get('[type=password]').type(user.password);
-    cy.get('.btn-lg').click();
+    cy.contains('a.nav-link', user.username.toLowerCase()).click();
+    cy.contains(article.title).should('be.visible');
   });
 
-  it('should delete an article', () => {
+  it('should delete the article', () => {
     cy.contains('a.nav-link', user.username.toLowerCase()).click();
-    cy.get(':nth-child(2) > .preview-link').click();
-    cy.contains('.btn.btn-outline-danger.btn-sm', 'Delete Article').click();
+    cy.contains(article.title).click();
 
+    cy.contains('.btn.btn-outline-danger.btn-sm', 'Delete Article').click();
     cy.on('window:confirm', (text) => {
       expect(text).to.contains('Do you really want to delete it?');
       return true;
     });
 
-    cy.contains('.navbar-brand', 'conduit');
+    cy.contains(article.title).should('not.exist');
   });
 });
