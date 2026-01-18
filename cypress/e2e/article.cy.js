107b5be13e3120972c8110b2fb03c818234a2936
Viktoriia <viktoriamyhailiak@gmail.com>
2026-01-18 18:03:05 +0200

    add a solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 describe('Article Flow', () => {
   const timestamp = Date.now();
 
@@ -7,33 +9,39 @@ describe('Article Flow', () => {
     password: 'Password123!'
   };
 
-  const article = {
-    title: `JavaScript ${timestamp}`,
-    description: `Test Description ${timestamp}`,
-    body: `Test Body ${timestamp}`
-  };
-
   before(() => {
     cy.login(user.email, user.username, user.password);
   });
 
   it('should create an article', () => {
-    cy.createArticle(article.title, article.description, article.body);
+    const articleTitle = `JavaScript ${timestamp}`;
+    const articleDescription = `Test Description ${timestamp}`;
+    const articleBody = `Test Body ${timestamp}`;
+
+    cy.createArticle(articleTitle, articleDescription, articleBody);
+
     cy.visit('/');
     cy.contains('a.nav-link', user.username.toLowerCase()).click();
-    cy.contains(article.title).should('be.visible');
+    cy.contains(articleTitle).should('be.visible');
   });
 
-  it('should delete the article', () => {
-    cy.contains('a.nav-link', user.username.toLowerCase()).click();
-    cy.contains(article.title).click();
+  it('should delete an article', () => {
+    const articleTitle = `JS Delete ${Date.now()}`;
+    const articleDescription = 'Description for deletion';
+    const articleBody = 'Body for deletion';
 
-    cy.contains('.btn.btn-outline-danger.btn-sm', 'Delete Article').click();
+    cy.createArticle(articleTitle, articleDescription, articleBody);
+
+    cy.visit('/');
+    cy.contains('a.nav-link', user.username.toLowerCase()).click();
+    cy.contains(articleTitle).click();
+
     cy.on('window:confirm', (text) => {
       expect(text).to.contains('Do you really want to delete it?');
       return true;
     });
 
-    cy.contains(article.title).should('not.exist');
+    cy.contains('.btn.btn-outline-danger.btn-sm', 'Delete Article').click();
+    cy.contains(articleTitle).should('not.exist');
   });
 });
